ba33e8d3e66ce907dd7aaba5bddcc4522b16f894
Bogdan Brl <bogdan.brlx@gmail.com>
2021-05-17 13:51:31 +0300

    fixing LazyInitializationException

---
+++ chapter_04/taco-cloud/src/test/java/tacos/controllers/HomeControllerTest.java
@@ -0,0 +1,27 @@
+package tacos.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest()
+class HomeControllerTest {
+
+//    @Autowired
+//    private MockMvc mockMvc;
+//
+//    @Test
+//    void testHomePage() throws Exception{
+//        mockMvc.perform(get("/"))
+//                .andExpect(status().isOk())
+//                .andExpect(view().name("home"))
+//                .andExpect(content().string(containsString("Welcome to...")));
+//    }
+}
